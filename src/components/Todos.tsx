9c303b807dc553c4110bc269ef43b5fe1dd58538
Soltan Abilgasimzada <soltan.ebilqasimzade@gmail.com>
2022-07-26 17:45:47 +0400

    Hide scrollbar

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -59,7 +59,7 @@ export const Todos = () => {
 
     return (
         <section className="h-screen flex items-center flex-col justify-center py-12 sm:px-6 lg:px-8">
-            <div className="max-w-md w-full p-3 bg-white dark:bg-slate-800 shadow overflow-x-hidden overflow-y-auto todo-list sm:rounded-lg space-y-8">
+            <div className="max-w-md w-full p-3 bg-white dark:bg-slate-800 shadow overflow-hidden sm:rounded-lg space-y-8">
                 <div className="flex justify-between">
                     <h1 className="font-medium dark:text-white">Todo App</h1>
                     <DarkModeSwitch
@@ -73,7 +73,7 @@ export const Todos = () => {
                          handleChange={handleChange}
                          task={task}
                 />
-                <div className="h-80 overflow-scroll">
+                <div className="h-80 overflow-x-hidden overflow-y-auto todo-list">
                     {todos.map((todo) => (
                         <Row key={todo.id}
                              todo={todo}
